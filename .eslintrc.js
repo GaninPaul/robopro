0999e72511717454f610daf31e379468c717cf7b
p.ganin <p.ganin@puple.studio>
2023-07-22 17:31:17 +0400

    feat: apply default configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'i18n'],
+  env: {
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [2, { bracketSpacing: true }],
+    'react-hooks/exhaustive-deps': 'off',
+    'no-shadow': 'off',
+
+    'react/jsx-sort-props': [
+      'error',
+      { shorthandFirst: true, callbacksLast: true },
+    ],
+    'react/jsx-curly-brace-presence': ['error', 'never'],
+    'react/jsx-boolean-value': ['error', 'never'],
+    'react/jsx-no-useless-fragment': 'error',
+    'react/jsx-fragments': 'warn',
+    'react/jsx-key': 'error',
+    'react/no-danger-with-children': 'error',
+    'react/no-array-index-key': 'warn',
+    'react/no-deprecated': 'warn',
+
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/ban-types': ['error', { types: { object: false } }],
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+    '@typescript-eslint/prefer-includes': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+
+    'i18n/no-russian-character': [
+      'error',
+      {
+        includeIdentifier: true,
+      },
+    ],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          config: './src/config',
+          types: './src/types',
+          navigation: './src/navigation',
+          components: './src/components',
+          services: './src/services',
+          utils: './src/utils',
+          constants: './src/constants',
+          stores: './src/stores',
+          screens: './src/screens',
+          theme: './src/theme',
+          assets: './src/assets',
+          hooks: './src/hooks',
+          controllers: './src/controllers',
+          tests: './src/tests',
+        },
+      },
+    ],
+  ],
 };
