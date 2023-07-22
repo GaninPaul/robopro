e7ce469e8ba6d04294b9352ece7e8df32cc74c61
p.ganin <p.ganin@puple.studio>
2023-07-23 00:02:39 +0400

    feat: add util throttle

---
+++ src/utils/throttle.ts
@@ -0,0 +1,24 @@
+export function throttle(cb: (..._: any) => any, delay = 1000) {
+  let shouldWait = false;
+  let waitingArgs: any;
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return (...args: any) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    cb(...args);
+    shouldWait = true;
+    setTimeout(timeoutFunc, delay);
+  };
+}
