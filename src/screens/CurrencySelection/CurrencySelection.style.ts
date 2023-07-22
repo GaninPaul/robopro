1e0c5def38039c3ad1aca0fc03e1f4173a8c2ef3
p.ganin <p.ganin@puple.studio>
2023-07-23 00:03:39 +0400

    feat: add CurrencySelection  screen

--- src/screens/CurrencySelection/CurrencySelection.style.ts
+++ src/screens/CurrencySelection/CurrencySelection.style.ts
@@ -2,10 +2,12 @@ import styled from 'styled-components/native';
 
 export const Footer = styled.View`
   display: flex;
-  position: absolute;
   flex-direction: column;
+  width: 100%;
 
   bottom: 0;
+
+  padding: 30px;
 `;
 
 export const Container = styled.View`
@@ -13,3 +15,21 @@ export const Container = styled.View`
   flex-direction: column;
   flex-grow: 1;
 `;
+
+export const Content = styled.View`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+
+  text-align: center;
+`;
+
+
--- src/screens/CurrencySelection/CurrencySelection.tsx
+++ src/screens/CurrencySelection/CurrencySelection.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import {Button} from '../../components/Button/Button';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
 
-import {Container, Footer} from './CurrencySelection.style';
+import { QuotesStoreInst } from 'stores/QuotesStore';
 
-export const CurrencySelection = () => {
+import { AppStackParams } from 'navigation/AppNavigator';
+import { APP_ROUTES } from 'navigation/AppRoutes';
+
+import { Symbols } from 'constants/symbols';
+
+import { Button } from 'components/Button/Button';
+import { Picker } from 'components/Picker/Picker';
+
+import { Container, Content, Footer, Title } from './CurrencySelection.style';
+
+interface Props {
+  navigation: StackNavigationProp<
+    AppStackParams,
+    APP_ROUTES.CURRENCY_SELECTION
+  >;
+}
+export const CurrencySelection = observer(({ navigation }: Props) => {
+  const { symbol, setSymbol, clear } = QuotesStoreInst;
+
   const onPress = () => {
-    console.log('onPress');
+    navigation.navigate(APP_ROUTES.QUOTES);
   };
+
+  useEffect(() => {
+    clear();
+  }, []);
+
   return (
     <Container>
+      <Content>
+        <Title>Select symbol</Title>
+        <Picker
+          currrentValue={symbol}
+          values={Symbols}
+          onChange={value => {
+            setSymbol(value);
+          }}
+        />
+      </Content>
       <Footer>
-        <Button onPress={onPress}>Check</Button>
+        <Button onPress={onPress}>Check quotes</Button>
       </Footer>
     </Container>
   );
-};
+});
