7137803a8678fd9ba69bb23cb351c564b5503330
p.ganin <p.ganin@puple.studio>
2023-07-23 00:04:29 +0400

    feat: add Quotes  screen

--- src/screens/Quotes/Quotes.style.ts
+++ src/screens/Quotes/Quotes.style.ts
@@ -1,15 +1,22 @@
 import styled from 'styled-components/native';
 
-export const Footer = styled.View`
-  display: flex;
-  position: absolute;
-  flex-direction: column;
-
-  bottom: 0;
-`;
-
 export const Container = styled.View`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+`;
+
+export const Content = styled.View`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.Text`
+  font-size: 20px;
+
+  text-align: center;
 `;
--- src/screens/Quotes/Quotes.tsx
+++ src/screens/Quotes/Quotes.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
 
-import {Button} from '../../components/Button/Button';
+import { QuotesStoreInst } from 'stores/QuotesStore';
 
-import {Container, Footer} from './Quotes.style';
+import { AppStackParams } from 'navigation/AppNavigator';
+import { APP_ROUTES } from 'navigation/AppRoutes';
 
-export const Quotes = () => {
-  const onPress = () => {
-    console.log('onPress');
-  };
+import { Chart } from './components/Chart/Chart';
+import { QuotesControllerInst } from './services/QuotesController';
+
+import { Container, Content, Text } from './Quotes.style';
+
+interface Props {
+  navigation: StackNavigationProp<AppStackParams, APP_ROUTES.QUOTES>;
+}
+export const Quotes = observer(({ navigation }: Props) => {
+  const {
+    chartLabels,
+    chartDataBidPrice,
+    chartDataAskPrice,
+    latestAskPrice,
+    symbol,
+    clear,
+    latestBidPrice,
+  } = QuotesStoreInst;
+
+  useEffect(() => {
+    navigation.setOptions({ title: 'Exchange Rates' });
+    QuotesControllerInst.start();
+    return () => {
+      QuotesControllerInst.stop();
+      clear();
+    };
+  }, []);
+
   return (
     <Container>
-      <Footer>
-        <Button onPress={onPress}>Check</Button>
-      </Footer>
+      <Content>
+        <Text>Сurrency: {symbol}</Text>
+        <Text>AskPrice: ${latestAskPrice}</Text>
+        <Text>BidPrice: ${latestBidPrice}</Text>
+        <Chart
+          data={{
+            labels: chartLabels,
+            datasets: [
+              { data: chartDataBidPrice },
+              { data: chartDataAskPrice },
+            ],
+          }}
+        />
+      </Content>
     </Container>
   );
-};
+});
